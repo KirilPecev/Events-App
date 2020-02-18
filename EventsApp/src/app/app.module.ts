00aed852ca0e38cb0d6d2d8eaf3262317d672783
Kiril Pecev <kir4o_pecev@abv.bg>
2020-02-18 23:34:11 +0200

    added bootstrap

--- EventsApp/src/app/app.module.ts
+++ EventsApp/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
