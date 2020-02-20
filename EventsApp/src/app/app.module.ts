52193759ad3b5faa88d5a4bc7fc85f21957e8b17
Kiril Pecev <kir4o_pecev@abv.bg>
2020-02-20 22:17:17 +0200

    added new component - publication

--- EventsApp/src/app/app.module.ts
+++ EventsApp/src/app/app.module.ts
@@ -8,11 +8,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HomePageFeedComponent } from './home-page-feed/home-page-feed.component';
+import { PublicationComponent } from './publication/publication.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageFeedComponent
+    HomePageFeedComponent,
+    PublicationComponent
   ],
   imports: [
     BrowserModule,
