583e73db4f84a72d06179f309ac20b51f9c9e41c
Kiril Pecev <kir4o_pecev@abv.bg>
2020-03-04 00:33:44 +0200

    updates

--- EventsApp/src/app/app-routing.module.ts
+++ EventsApp/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+
 import { RegisterComponent } from './register/register.component';
 import { HomePageIndexComponent } from './home-page-index/home-page-index.component';
 import { ProfileComponent } from './profile/profile.component';
--- EventsApp/src/app/app.module.ts
+++ EventsApp/src/app/app.module.ts
@@ -5,8 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HomePageFeedComponent } from './home-page-feed/home-page-feed.component';
 import { PublicationComponent } from './publication/publication.component';
 import { HeaderComponent } from './header/header.component';
@@ -14,15 +12,13 @@ import { LeftSidebarComponent } from './left-sidebar/left-sidebar.component';
 import { RightSidebarComponent } from './right-sidebar/right-sidebar.component';
 import { HomePageIndexComponent } from './home-page-index/home-page-index.component';
 import { FooterComponent } from './footer/footer.component';
-import { MatCardModule } from '@angular/material/card';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-import { MatInputModule } from '@angular/material/input';
-import { MatBadgeModule } from '@angular/material/badge';
-import { MatDialogModule } from '@angular/material/dialog';
 import { NotificationsComponent } from './notifications/notifications.component';
 import { FriendsComponent } from './friends/friends.component';
 import { ProfileComponent } from './profile/profile.component';
+
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -33,7 +29,7 @@ import { ProfileComponent } from './profile/profile.component';
     LeftSidebarComponent,
     RightSidebarComponent,
     HomePageIndexComponent,
-  FooterComponent,
+    FooterComponent,
     LoginComponent,
     RegisterComponent,
     NotificationsComponent,
@@ -44,15 +40,9 @@ import { ProfileComponent } from './profile/profile.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
-    FontAwesomeModule,
-    MatCardModule,
-    MatInputModule,
-    MatBadgeModule,
-    MatDialogModule
+    SharedModule
   ],
   providers: [],
-  bootstrap: [AppComponent],
-  entryComponents: [NotificationsComponent, FriendsComponent]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
