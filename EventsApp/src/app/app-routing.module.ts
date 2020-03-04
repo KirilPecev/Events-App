240b7e55d6c0b3649effc1ddaf1287af735dc641
Kiril Pecev <kir4o_pecev@abv.bg>
2020-03-05 00:50:19 +0200

    added multiple router outlets

--- EventsApp/src/app/app-routing.module.ts
+++ EventsApp/src/app/app-routing.module.ts
@@ -5,13 +5,28 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { HomePageIndexComponent } from './home-page-index/home-page-index.component';
 import { ProfileComponent } from './profile/profile.component';
+import { DaybookComponent } from './daybook/daybook.component';
+import { UserInformationComponent } from './user-information/user-information.component';
+import { UserBasicInformationComponent } from './user-basic-information/user-basic-information.component';
+import { UserContactsComponent } from './user-contacts/user-contacts.component';
 
 
 const routes: Routes = [
   { path: "", component: HomePageIndexComponent },
   { path: "login", component: LoginComponent },
   { path: "register", component: RegisterComponent },
-  { path: "profile", component: ProfileComponent }
+  {
+    path: "profile", component: ProfileComponent, children: [
+      { path: "", component: DaybookComponent, outlet: "profile" },
+      {
+        path: "information", component: UserInformationComponent, outlet: "profile", children: [
+          { path: "", component: UserContactsComponent, outlet: "info" },
+          { path: "contacts", component: UserContactsComponent, outlet: "info" },
+          { path: "basic-info", component: UserBasicInformationComponent, outlet: "info" }
+        ]
+      }
+    ]
+  }
 ];
 
 @NgModule({
