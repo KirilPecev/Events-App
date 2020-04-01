00313fca40e0e8a7ce853de5b0523c64c9630657
Kiril Pecev <kir4o_pecev@abv.bg>
2020-04-01 15:28:56 +0300

    added flex responsive design

--- EventsApp/src/app/positions/positions.component.ts
+++ EventsApp/src/app/positions/positions.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-positions',
-  templateUrl: './positions.component.html',
-  styleUrls: ['./positions.component.css']
+  selector: "app-positions",
+  templateUrl: "./positions.component.html",
+  styleUrls: ["./positions.component.css"]
 })
 export class PositionsComponent implements OnInit {
-  panelOpenState = true;
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
