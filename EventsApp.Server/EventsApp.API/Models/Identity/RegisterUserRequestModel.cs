e3c779f5984f9b4b81aaf211c8350f6a884ef59c
Kiril Pecev <kir4o_pecev@abv.bg>
2020-04-15 16:22:13 +0300

    added validation

--- EventsApp.Server/EventsApp.API/Models/Identity/RegisterUserRequestModel.cs
+++ EventsApp.Server/EventsApp.API/Models/Identity/RegisterUserRequestModel.cs
@@ -2,12 +2,18 @@
 {
     using System.ComponentModel.DataAnnotations;
 
+    using static Data.ValidationConstants.User;
+
     public class RegisterUserRequestModel
     {
         [Required]
+        [MaxLength(FirstNameMaxLength)]
+        [MinLength(FirstNameMinimumLength)]
         public string FirstName { get; set; }
 
         [Required]
+        [MaxLength(LastNameMaxLength)]
+        [MinLength(LastNameMinimumLength)]
         public string LastName { get; set; }
 
         [Required]
