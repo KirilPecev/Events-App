c3b5f89673cff25285391fc3f7bd6704ae4b7df3
Kiril Pecev <kir4o_pecev@abv.bg>
2020-04-15 19:50:39 +0300

    added service collection extensions

---
+++ EventsApp.Server/EventsApp.API/Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,76 @@
+﻿namespace EventsApp.API.Infrastructure.Extensions
+{
+    using Data;
+    using Data.Models;
+    using Features.Identity;
+    using Microsoft.AspNetCore.Authentication.JwtBearer;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.IdentityModel.Tokens;
+    using System.Text;
+
+    public static class ServiceCollectionExtensions
+    {
+        public static AppSettings GetAppSettings(this IServiceCollection services, IConfiguration configuration)
+        {
+
+            IConfigurationSection appSettingsConfiguration = configuration.GetSection("AppSettings");
+            services.Configure<AppSettings>(appSettingsConfiguration);
+
+            return appSettingsConfiguration.Get<AppSettings>();
+        }
+
+        public static IServiceCollection AddDatabase(this IServiceCollection services, IConfiguration configuration)
+                 => services
+                    .AddDbContext<EvenityDbContext>(options => options
+                        .UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+        public static IServiceCollection AddIdentity(this IServiceCollection services)
+        {
+            services
+                .AddIdentity<User, IdentityRole>(opt =>
+                {
+                    opt.Password.RequiredLength = 4;
+                    opt.Password.RequireDigit = false;
+                    opt.Password.RequireUppercase = false;
+                    opt.Password.RequireLowercase = false;
+                    opt.Password.RequireNonAlphanumeric = false;
+                })
+                .AddEntityFrameworkStores<EvenityDbContext>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddJwtAuthentication(this IServiceCollection services, AppSettings appSettings)
+        {
+            byte[] key = Encoding.ASCII.GetBytes(appSettings.Secret);
+
+            services
+                .AddAuthentication(x =>
+                {
+                    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+                .AddJwtBearer(x =>
+                {
+                    x.RequireHttpsMetadata = false;
+                    x.SaveToken = true;
+                    x.TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(key),
+                        ValidateIssuer = false,
+                        ValidateAudience = false
+                    };
+                });
+
+            return services;
+        }
+
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+            => services
+                .AddTransient<IIdentityService, IdentityService>();
+    }
+}
--- EventsApp.Server/EventsApp.API/Startup.cs
+++ EventsApp.Server/EventsApp.API/Startup.cs
@@ -1,18 +1,11 @@
 namespace EventsApp.API
 {
-    using Data;
-    using Data.Models;
-    using Microsoft.AspNetCore.Authentication.JwtBearer;
+    using Infrastructure.Extensions;
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
-    using Microsoft.AspNetCore.Identity;
-    using Microsoft.EntityFrameworkCore;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Hosting;
-    using Microsoft.IdentityModel.Tokens;
-    using System.Text;
-    using Infrastructure.Extensions;
 
     public class Startup
     {
@@ -23,46 +16,11 @@ namespace EventsApp.API
         public void ConfigureServices(IServiceCollection services)
         {
             services
-                .AddDbContext<EvenityDbContext>(options => options
-                    .UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-
-            services
-                .AddIdentity<User, IdentityRole>(opt =>
-                {
-                    opt.Password.RequiredLength = 4;
-                    opt.Password.RequireDigit = false;
-                    opt.Password.RequireUppercase = false;
-                    opt.Password.RequireLowercase = false;
-                    opt.Password.RequireNonAlphanumeric = false;
-                })
-                .AddEntityFrameworkStores<EvenityDbContext>();
-
-            IConfigurationSection appSettingsConfiguration = Configuration.GetSection("AppSettings");
-            services.Configure<AppSettings>(appSettingsConfiguration);
-
-            AppSettings appSettings = appSettingsConfiguration.Get<AppSettings>();
-            byte[] key = Encoding.ASCII.GetBytes(appSettings.Secret);
-
-            services
-                .AddAuthentication(x =>
-                {
-                    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-                })
-                .AddJwtBearer(x =>
-                {
-                    x.RequireHttpsMetadata = false;
-                    x.SaveToken = true;
-                    x.TokenValidationParameters = new TokenValidationParameters()
-                    {
-                        ValidateIssuerSigningKey = true,
-                        IssuerSigningKey = new SymmetricSecurityKey(key),
-                        ValidateIssuer = false,
-                        ValidateAudience = false
-                    };
-                });
-
-            services.AddControllers();
+                .AddDatabase(this.Configuration)
+                .AddIdentity()
+                .AddJwtAuthentication(services.GetAppSettings(this.Configuration))
+                .AddApplicationServices()
+                .AddControllers();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -72,19 +30,18 @@ namespace EventsApp.API
                 app.UseDatabaseErrorPage();
             }
 
-            app.UseRouting();
-
-            app.UseCors(opt => opt
-                .AllowAnyHeader()
-                .AllowAnyMethod()
-                .AllowAnyOrigin());
-
-            app.UseAuthentication();
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
-
-            app.ApplyMigrations();
+            app.UseRouting()
+                .UseCors(opt => opt
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .AllowAnyOrigin())
+                .UseAuthentication()
+                .UseAuthorization()
+                .UseEndpoints(endpoints =>
+                {
+                    endpoints.MapControllers();
+                })
+                .ApplyMigrations();
         }
     }
 }
