ee3e419576260cbdaeec53f9531b1c126249f792
Kiril Pecev <kir4o_pecev@abv.bg>
2020-04-22 17:16:54 +0300

    added position and event data models

---
+++ EventsApp.Server/EventsApp.API/Data/Models/Event.cs
@@ -0,0 +1,36 @@
+﻿namespace EventsApp.API.Data.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Event : BaseModel<int>
+    {
+        public Event()
+        {
+            this.Positions = new List<Position>();
+        }
+        
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Sport { get; set; }
+
+        [Required]
+        public string Location { get; set; }
+
+        [Required]
+        public DateTime DateTime { get; set; }
+
+        public int Likes { get; set; }
+
+        public int Shares { get; set; }
+
+        [Required]
+        public string CreatorId { get; set; }
+        public User Creator { get; set; }
+
+        public ICollection<Position> Positions { get; set; }
+    }
+}
---
+++ EventsApp.Server/EventsApp.API/Data/Models/Position.cs
@@ -0,0 +1,14 @@
+﻿namespace EventsApp.API.Data.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Position : BaseModel<int>
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string ParticipantId { get; set; }
+        public User Participant { get; set; }
+    }
+}
