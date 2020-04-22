6c7a0c24075e58b69ea0a6166b9aa4733b590845
Kiril Pecev <kir4o_pecev@abv.bg>
2020-04-22 16:45:37 +0300

    added publication data model

---
+++ EventsApp.Server/EventsApp.API/Data/Models/BaseModel.cs
@@ -0,0 +1,10 @@
+﻿namespace EventsApp.API.Data.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class BaseModel<T>
+    {
+        [Key]
+        public T Id { get; set; }
+    }
+}
---
+++ EventsApp.Server/EventsApp.API/Data/Models/Publication.cs
@@ -0,0 +1,20 @@
+﻿namespace EventsApp.API.Data.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Publication : BaseModel<int>
+    {
+        [Required]
+        public string Description { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public int Likes { get; set; }
+
+        public int Shares { get; set; }
+
+        [Required]
+        public string CreatorId { get; set; }
+        public User Creator { get; set; }
+    }
+}
