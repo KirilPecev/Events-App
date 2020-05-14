4e919aad884db65860d31678fcc864bdfd1cb782
Kiril Pecev <kir4o_pecev@abv.bg>
2020-05-14 13:29:09 +0300

    added create publication functionality

--- EventsApp.Client/src/app/shared/publication/share-publication/share-publication.component.ts
+++ EventsApp.Client/src/app/shared/publication/share-publication/share-publication.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { PublicationService } from "src/app/core/services/publication.service";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 
 @Component({
   selector: "app-share-publication",
@@ -7,7 +8,22 @@ import { PublicationService } from "src/app/core/services/publication.service";
   styleUrls: ["./share-publication.component.css"],
 })
 export class SharePublicationComponent implements OnInit {
-  constructor(private publicationService: PublicationService) {}
+  publicationForm: FormGroup;
+
+  constructor(fb: FormBuilder, private publicationService: PublicationService) {
+    this.publicationForm = fb.group({
+      description: [""],
+    });
+  }
 
   ngOnInit(): void {}
+
+  share() {
+    var data: any = {
+      description: this.publicationForm.value["description"],
+    };
+    this.publicationService.create(data).subscribe((data) => {
+      this.publicationForm.reset();
+    });
+  }
 }
