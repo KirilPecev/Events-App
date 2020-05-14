39e59a3f2994295cc3be84725d4a625814a4fd58
Kiril Pecev <kir4o_pecev@abv.bg>
2020-05-14 15:13:56 +0300

    added new methods to fetch publications

--- EventsApp.Client/src/app/shared/publication/publication.component.ts
+++ EventsApp.Client/src/app/shared/publication/publication.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { PublicationService } from "src/app/core/services/publication.service";
 import { Publication } from "src/app/core/models/publication-model";
 import { Observable } from "rxjs";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: "app-publication",
@@ -10,21 +11,46 @@ import { Observable } from "rxjs";
 })
 export class PublicationComponent implements OnInit {
   publications$: Observable<Array<Publication>>;
-  constructor(private publicationService: PublicationService) {}
+  constructor(
+    private publicationService: PublicationService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.fetch();
+  }
+
+  fetch() {
+    if (this.route.outlet === "dashboard") {
+      this.fetchPublications();
+    } else {
+      this.fetchMinePublications();
+    }
+  }
+
+  fetchPublications() {
     this.publications$ = this.publicationService.getPublications();
   }
 
+  fetchMinePublications() {
+    this.publications$ = this.publicationService.getMinePublications();
+  }
+
   like(id) {
-    this.publicationService.like(id).subscribe((data) => {});
+    this.publicationService.like(id).subscribe((data) => {
+      this.fetch();
+    });
   }
 
   unlike(id) {
-    this.publicationService.unlike(id).subscribe((data) => {});
+    this.publicationService.unlike(id).subscribe((data) => {
+      this.fetch();
+    });
   }
 
   share(id) {
-    this.publicationService.share(id).subscribe((data) => {});
+    this.publicationService.share(id).subscribe((data) => {
+      this.fetch();
+    });
   }
 }
