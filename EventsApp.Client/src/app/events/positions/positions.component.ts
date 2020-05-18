3d6950c8d5885786cc58ee600c34ba5308b88989
Kiril Pecev <kir4o_pecev@abv.bg>
2020-05-18 22:27:34 +0300

    added eventId input property

--- EventsApp.Client/src/app/events/positions/positions.component.ts
+++ EventsApp.Client/src/app/events/positions/positions.component.ts
@@ -3,11 +3,10 @@ import { Component, OnInit, Input } from "@angular/core";
 @Component({
   selector: "app-positions",
   templateUrl: "./positions.component.html",
-  styleUrls: ["./positions.component.css"]
+  styleUrls: ["./positions.component.css"],
 })
 export class PositionsComponent implements OnInit {
-
-  @Input() availablePositions: Array<string>;
+  @Input() eventId: number;
   constructor() {}
 
   ngOnInit(): void {}
