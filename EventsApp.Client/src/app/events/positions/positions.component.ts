a9d2f5a4ee9ffcce9674e44fb3764ebf11c9a9da
Kiril Pecev <kir4o_pecev@abv.bg>
2020-05-20 22:55:58 +0300

    added functionality about positions

--- EventsApp.Client/src/app/events/positions/positions.component.ts
+++ EventsApp.Client/src/app/events/positions/positions.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, Input } from "@angular/core";
+import { PositionService } from "../../core/services/position.service";
+import { Observable } from "rxjs";
+import { Position } from "../../core/models/position-model";
 
 @Component({
   selector: "app-positions",
@@ -7,7 +10,21 @@ import { Component, OnInit, Input } from "@angular/core";
 })
 export class PositionsComponent implements OnInit {
   @Input() eventId: number;
-  constructor() {}
+  constructor(private positionService: PositionService) {}
 
-  ngOnInit(): void {}
+  availablePositions$: Observable<Array<Position>>;
+  busyPositions$: Observable<Array<Position>>;
+
+  ngOnInit(): void {
+    this.availablePositions$ = this.positionService.getAvailablePositions(this.eventId);
+    this.busyPositions$ = this.positionService.getBusyPositions(this.eventId);
+  }
+
+  join(id){
+
+  }
+
+  quit(id){
+    
+  }
 }
