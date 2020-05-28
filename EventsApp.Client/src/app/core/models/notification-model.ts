0a6e67dc9ec3797e421840bfdd98fc0907c87ebc
Kiril Pecev <kir4o_pecev@abv.bg>
2020-05-28 15:50:44 +0300

    added notification model; added notification service;

---
+++ EventsApp.Client/src/app/core/models/notification-model.ts
@@ -0,0 +1,5 @@
+export interface Notification {
+  description: string;
+  imageUrl: string;
+  user: string;
+}
---
+++ EventsApp.Client/src/app/core/services/notification.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpParams } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { environment } from "../../../environments/environment";
+import { Notification } from "../models/notification-model";
+
+@Injectable({
+  providedIn: "root",
+})
+export class NotificationService {
+  private notificationPath = environment.apiUrl + "notification";
+
+  constructor(private http: HttpClient) {}
+
+  getNotifications(): Observable<Array<Notification>> {
+    return this.http.get<Array<Notification>>(
+      this.notificationPath + "/byuser"
+    );
+  }
+}
