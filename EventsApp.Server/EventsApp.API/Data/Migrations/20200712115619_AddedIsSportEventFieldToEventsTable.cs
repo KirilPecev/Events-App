e9f3716f2d97bfb3433f4022f9002398d8430eec
Kiril Pecev <kir4o_pecev@abv.bg>
2020-07-12 15:25:24 +0300

    added IsSportEvent field in events table

---
+++ EventsApp.Server/EventsApp.API/Data/Migrations/20200712115619_AddedIsSportEventFieldToEventsTable.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EventsApp.API.Data.Migrations
+{
+    public partial class AddedIsSportEventFieldToEventsTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsSportEvent",
+                table: "Events",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsSportEvent",
+                table: "Events");
+        }
+    }
+}
