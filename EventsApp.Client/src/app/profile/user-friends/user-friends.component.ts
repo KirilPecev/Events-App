062140c66392d1ceb4d9bb8624c82ff56440cd32
Kiril Pecev <kir4o_pecev@abv.bg>
2020-07-16 15:14:50 +0300

    added my friends count

--- EventsApp.Client/src/app/profile/user-friends/user-friends.component.ts
+++ EventsApp.Client/src/app/profile/user-friends/user-friends.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Input } from "@angular/core";
 import { faUsers } from "@fortawesome/free-solid-svg-icons";
 import { Observable } from "rxjs";
 import { Friend } from "../../core/models/friend-model";
 import { UserService } from "../../core/services/user.service";
 import { ActivatedRoute } from "@angular/router";
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: "app-user-friends",
@@ -13,6 +14,8 @@ import { ActivatedRoute } from "@angular/router";
 export class UserFriendsComponent implements OnInit {
   friends$: Observable<Array<Friend>>;
   faUsers = faUsers;
+
+  myFriendsCount: number;
   constructor(
     private userService: UserService,
     private route: ActivatedRoute
@@ -20,6 +23,11 @@ export class UserFriendsComponent implements OnInit {
 
   ngOnInit(): void {
     let userId = this.route.snapshot.pathFromRoot[2].params["userId"];
-    this.friends$ = this.userService.getFriends(userId);
+    this.friends$ = this.userService.getFriends(userId)
+    .pipe(
+      tap(data => {
+        this.myFriendsCount = data.length;
+      })
+    );
   }
 }
