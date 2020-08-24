9f129111286a3f0919ef95362ac14ef227683a55
Kiril Pecev <kir4o_pecev@abv.bg>
2020-08-24 18:46:08 +0300

    prod env update

--- EventsApp.Client/src/environments/environment.prod.ts
+++ EventsApp.Client/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: "https://eventsapikp.azurewebsites.net/api/",
+  firebase: {
+    apiKey: "AIzaSyCt7kx5TI0EBnScWTC6FMVZvIW3EYCRcgU",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "evenity-ea94e",
+    storageBucket: "gs://evenity-ea94e.appspot.com",
+    messagingSenderId: "",
+  },
 };
