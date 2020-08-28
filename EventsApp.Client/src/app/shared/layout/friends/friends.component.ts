c758c6fc862d5ad8dce885b75412f4526f5a2aaf
Kiril Pecev <kir4o_pecev@abv.bg>
2020-08-29 00:17:25 +0300

    added accept friend notification

--- EventsApp.Client/src/app/shared/layout/friends/friends.component.ts
+++ EventsApp.Client/src/app/shared/layout/friends/friends.component.ts
@@ -3,6 +3,7 @@ import { UserService } from "../../../core/services/user.service";
 import { Observable } from "rxjs";
 import { Friend } from "src/app/core/models/friend-model";
 import { MAT_DIALOG_DATA } from "@angular/material/dialog";
+import { NotificationService } from "src/app/core/services/notification.service";
 
 @Component({
   selector: "app-friends",
@@ -13,6 +14,7 @@ export class FriendsComponent implements OnInit {
   friends$: Observable<Array<Friend>>;
   constructor(
     private userService: UserService,
+    private notificationService: NotificationService,
     @Inject(MAT_DIALOG_DATA) public data: any
   ) {}
 
@@ -29,7 +31,17 @@ export class FriendsComponent implements OnInit {
   accept(friendId: string) {
     this.userService.acceptFriendship(friendId).subscribe((data) => {
       this.fetch();
+      this.createNotification(friendId);
     });
+  }
+
+  private createNotification(friendId: string) {
+    const data = {
+      description: `${this.userService.getUserFullName()} is now a friend`,
+      userId: friendId,
+    };
+
+    this.notificationService.create(data).subscribe();
   }
 
   remove(friendId: string) {
