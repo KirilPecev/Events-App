961deb087e97f07f4bde5b7a76fb6af6159d6da9
Kiril Pecev <kir4o_pecev@abv.bg>
2020-09-07 18:09:53 +0300

    added styling

--- EventsApp.Client/src/app/profile/user-settings/user-settings.component.ts
+++ EventsApp.Client/src/app/profile/user-settings/user-settings.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { faCog, faShieldAlt } from "@fortawesome/free-solid-svg-icons";
 @Component({
-  selector: 'app-user-settings',
-  templateUrl: './user-settings.component.html',
-  styleUrls: ['./user-settings.component.css']
+  selector: "app-user-settings",
+  templateUrl: "./user-settings.component.html",
+  styleUrls: ["./user-settings.component.css"],
 })
 export class UserSettingsComponent implements OnInit {
+  faCog = faCog;
+  faShieldAlt = faShieldAlt;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
