145d97b1b79803308ff86e04ae2085280bc667cb
Kiril Pecev <kir4o_pecev@abv.bg>
2020-09-08 17:03:51 +0300

    added general setting component and security-settings component

--- EventsApp.Client/src/app/settings/settings-routing.module.ts
+++ EventsApp.Client/src/app/settings/settings-routing.module.ts
@@ -3,12 +3,26 @@ import { Routes, RouterModule } from "@angular/router";
 
 import { SettingsComponent } from "./settings.component";
 import { AuthGuard } from "../guards/auth/auth.guard";
+import { GeneralSettingsComponent } from './general-settings/general-settings.component';
+import { SecuritySettingsComponent } from './security-settings/security-settings.component';
 
 const settingsRoutes: Routes = [
   {
     path: ":userId",
     component: SettingsComponent,
     canActivateChild: [AuthGuard],
+    children: [
+      {
+        path: "account",
+        component: GeneralSettingsComponent,
+        outlet: "settings"
+      },
+      {
+        path: "security",
+        component: SecuritySettingsComponent,
+        outlet: "settings"
+      }
+    ]
   },
 ];
 
--- EventsApp.Client/src/app/settings/settings.module.ts
+++ EventsApp.Client/src/app/settings/settings.module.ts
@@ -3,9 +3,15 @@ import { NgModule } from "@angular/core";
 import { SharedModule } from "../shared/shared.module";
 import { SettingsRoutingModule } from "./settings-routing.module";
 import { SettingsComponent } from "./settings.component";
+import { GeneralSettingsComponent } from "./general-settings/general-settings.component";
+import { SecuritySettingsComponent } from "./security-settings/security-settings.component";
 
 @NgModule({
   imports: [SharedModule, SettingsRoutingModule],
-  declarations: [SettingsComponent],
+  declarations: [
+    SettingsComponent,
+    GeneralSettingsComponent,
+    SecuritySettingsComponent,
+  ],
 })
 export class SettingsModule {}
