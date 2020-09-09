0326dcfc5fd8b23de9ce199258e609c1d4bdeb3d
Kiril Pecev <kir4o_pecev@abv.bg>
2020-09-09 14:07:42 +0300

    removed edit form

--- EventsApp.Client/src/app/profile/user-basic-information/user-basic-information.component.ts
+++ EventsApp.Client/src/app/profile/user-basic-information/user-basic-information.component.ts
@@ -3,8 +3,6 @@ import { User } from "../../core/models/user-model";
 import { Observable } from "rxjs";
 import { ActivatedRoute } from "@angular/router";
 import { UserService } from "../../core/services/user.service";
-import { FormGroup, FormBuilder, Validators } from "@angular/forms";
-import { tap } from "rxjs/operators";
 
 @Component({
   selector: "app-user-basic-information",
@@ -13,20 +11,11 @@ import { tap } from "rxjs/operators";
 })
 export class UserBasicInformationComponent implements OnInit {
   information$: Observable<User>;
-  editForm: FormGroup;
-  isMyProfile: boolean;
 
   constructor(
-    fb: FormBuilder,
     private userService: UserService,
     private route: ActivatedRoute
-  ) {
-    this.editForm = fb.group({
-      birthday: ["", [Validators.minLength(2)]],
-      gender: ["", [Validators.minLength(2)]],
-      favoriteSport: ["", [Validators.minLength(2)]],
-    });
-  }
+  ) {}
 
   ngOnInit(): void {
     this.getInformation();
@@ -34,21 +23,6 @@ export class UserBasicInformationComponent implements OnInit {
 
   getInformation() {
     let userId = this.route.snapshot.pathFromRoot[2].params["userId"];
-    this.isMyProfile = this.userService.getUserId() === userId;
-    this.information$ = this.userService
-      .getUserInformation(userId)
-      .pipe(tap(data => {
-        this.editForm.patchValue(data);
-      }));
-  }
-
-  edit() {
-    var data: any = {
-      birthday: this.editForm.value["birthday"],
-      gender: this.editForm.value["gender"],
-      favoriteSport: this.editForm.value["favoriteSport"],
-    };
-
-    this.userService.update(data).subscribe((data) => {});
+    this.information$ = this.userService.getUserInformation(userId);
   }
 }
--- EventsApp.Client/src/app/profile/user-contacts/user-contacts.component.ts
+++ EventsApp.Client/src/app/profile/user-contacts/user-contacts.component.ts
@@ -3,8 +3,6 @@ import { ActivatedRoute } from "@angular/router";
 import { Observable } from "rxjs";
 import { User } from "../../core/models/user-model";
 import { UserService } from "../../core/services/user.service";
-import { FormGroup, FormBuilder, Validators } from "@angular/forms";
-import { tap } from "rxjs/operators";
 
 @Component({
   selector: "app-user-contacts",
@@ -12,20 +10,12 @@ import { tap } from "rxjs/operators";
   styleUrls: ["./user-contacts.component.css"],
 })
 export class UserContactsComponent implements OnInit {
-  editForm: FormGroup;
   contacts$: Observable<User>;
-  isMyProfile: boolean;
 
   constructor(
-    fb: FormBuilder,
     private userService: UserService,
     private route: ActivatedRoute
-  ) {
-    this.editForm = fb.group({
-      mobile: ["", [Validators.minLength(2)]],
-      facebookUrl: ["", [Validators.minLength(2)]],
-    });
-  }
+  ) {}
 
   ngOnInit(): void {
     this.getInformation();
@@ -33,20 +23,6 @@ export class UserContactsComponent implements OnInit {
 
   getInformation() {
     let userId = this.route.snapshot.pathFromRoot[2].params["userId"];
-    this.isMyProfile = this.userService.getUserId() === userId;
-    this.contacts$ = this.userService.getUserInformation(userId).pipe(
-      tap((data) => {
-        this.editForm.patchValue(data);
-      })
-    );
-  }
-
-  edit() {
-    var data: any = {
-      mobile: this.editForm.value["mobile"],
-      facebookUrl: this.editForm.value["facebookUrl"],
-    };
-
-    this.userService.update(data).subscribe((data) => {});
+    this.contacts$ = this.userService.getUserInformation(userId);
   }
 }
