1249ebba42a33c9b94d63dce69ca65ecbd48871b
Kiril Pecev <kir4o_pecev@abv.bg>
2020-09-10 13:41:30 +0300

    added ui to security-settings.component

--- EventsApp.Client/src/app/settings/security-settings/security-settings.component.ts
+++ EventsApp.Client/src/app/settings/security-settings/security-settings.component.ts
@@ -1,15 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Validators, FormBuilder, FormGroup } from "@angular/forms";
+import { passwordMatch } from "src/app/shared/validators";
+import { UserService } from "src/app/core/services/user.service";
+import { ActivatedRoute, Router } from "@angular/router";
+import { ToastrService } from "ngx-toastr";
+import { User } from "src/app/core/validation-constants";
 
 @Component({
-  selector: 'app-security-settings',
-  templateUrl: './security-settings.component.html',
-  styleUrls: ['./security-settings.component.css']
+  selector: "app-security-settings",
+  templateUrl: "./security-settings.component.html",
+  styleUrls: ["./security-settings.component.css"],
 })
 export class SecuritySettingsComponent implements OnInit {
+  editForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    fb: FormBuilder,
+    private userService: UserService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private toastrService: ToastrService
+  ) {
+    this.editForm = fb.group({
+      email: [
+        "",
+        [
+          Validators.required,
+          Validators.minLength(User.FIRST_NAME_MIN_LENGTH),
+          Validators.maxLength(User.FIRST_NAME_MAX_LENGTH),
+        ],
+      ],
+      passwords: fb.group(
+        {
+          password: [
+            "",
+            [
+              Validators.required,
+              Validators.minLength(User.PASSWORD_MIN_LENGTH),
+            ],
+          ],
+          confirmPassword: ["", [Validators.required]],
+        },
+        { validators: [passwordMatch] }
+      ),
+    });
   }
 
+  ngOnInit(): void {}
+
+  edit(){
+
+  }
 }
