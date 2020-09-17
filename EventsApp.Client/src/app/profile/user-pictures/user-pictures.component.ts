697aaea08eeb1991586543a48c13bbc532234284
Kiril Pecev <kir4o_pecev@abv.bg>
2020-09-17 16:38:22 +0300

    UI improvements

--- EventsApp.Client/src/app/profile/user-pictures/user-pictures.component.ts
+++ EventsApp.Client/src/app/profile/user-pictures/user-pictures.component.ts
@@ -2,8 +2,9 @@ import { Component, OnInit } from "@angular/core";
 import { faImages } from "@fortawesome/free-solid-svg-icons";
 import { ActivatedRoute } from "@angular/router";
 import { PictureService } from "src/app/core/services/picture.service";
-import { ToastrService } from 'ngx-toastr';
-import { Profile } from 'src/app/core/message-constants';
+import { ToastrService } from "ngx-toastr";
+import { Profile } from "src/app/core/message-constants";
+import { UserService } from 'src/app/core/services/user.service';
 
 @Component({
   selector: "app-user-pictures",
@@ -14,15 +15,18 @@ export class UserPicturesComponent implements OnInit {
   faImages = faImages;
   userId: string;
   images: any[];
+  isMyProfile: boolean;
 
   constructor(
     private route: ActivatedRoute,
     private pictureService: PictureService,
+    private userService: UserService,
     private toastrService: ToastrService
   ) {}
 
   ngOnInit(): void {
     this.userId = this.route.snapshot.pathFromRoot[2].params["userId"];
+    this.isMyProfile = this.userService.getUserId() == this.userId;
     this.fetch();
   }
 
