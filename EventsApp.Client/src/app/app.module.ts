944fb8e064e816bf8d632bbb8aa67b6abef4b83f
Kiril Pecev <kir4o_pecev@abv.bg>
2022-06-24 22:37:30 +0300

    Migration finished

--- EventsApp.Client/src/app/app.module.ts
+++ EventsApp.Client/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { ToastrModule } from "ngx-toastr";
 
-import { AngularFireModule } from "@angular/fire";
+import { AngularFireModule } from "@angular/fire/compat";
 import { environment } from "../environments/environment";
 
 import { AppRoutingModule } from "./app-routing.module";
